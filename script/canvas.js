7396da634b7cdb2d92725fee2e7edd87f568df54
ShizeMation <1732145@gmail.com>
2019-03-12 03:12:54 -0400

    added more wiggles

--- script/canvas.js
+++ script/canvas.js
@@ -10,6 +10,7 @@ resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
 
 // drawing context
+/** @type {CanvasRenderingContext2D} */
 const c = canvas.getContext("2d");
 
 // ball object
@@ -20,38 +21,41 @@ function Ball(x, y) {
     this.r = Math.random()*40 + 5;
     this.dx = Math.random()*6 - 3;
     this.dy = Math.random()*6 - 3;
-
-    this.red = Math.floor(Math.random()*128) + 128;
-    this.green = Math.floor(Math.random()*128) + 128;
-    this.blue = Math.floor(Math.random()*128) + 128;
+    this.q = 10;
+    
+    this.red = Math.floor(Math.random()*256);
+    this.green = Math.floor(Math.random()*256);
+    this.blue = Math.floor(Math.random()*256);
 
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.r, 0, Math.PI*2);
         c.fillStyle = "rgba("+this.red+","+this.green+","+this.blue+",0.4)";
         c.fill();
-    }
+    };
 
     this.update = function() {
         this.x += this.dx;
         this.y += this.dy;
+        this.dx += Math.random()*4 - 2;
+        this.dy += Math.random()*4 - 2;
         if (this.x + this.r > canvas.width) {
             this.x = canvas.width - this.r;
-            this.dx = -this.dx;
+            this.dx = -this.dx/this.q;
         }
         else if (this.x - this.r < 0) {
             this.x = this.r;
-            this.dx = -this.dx;
+            this.dx = -this.dx/this.q;
         }
         if (this.y + this.r > canvas.height) {
             this.y = canvas.height - this.r;
-            this.dy = -this.dy;
+            this.dy = -this.dy/this.q;
         }
         else if (this.y - this.r < 0) {
             this.y = this.r;
-            this.dy = -this.dy;
+            this.dy = -this.dy/this.q;
         }
-    }
+    };
 }
 
 // balls are stored in the array
@@ -70,7 +74,9 @@ canvas.addEventListener("click", spawnBall);
 // animator
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
+    if (Date.now() % 120000 < 60000) {
+        c.clearRect(0, 0, canvas.width, canvas.height);
+    }
     for (var i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
