0b213f3d8f06b3a702191ba522c716ecdf1b8735
ShizeMation <1732145@gmail.com>
2020-07-24 23:51:43 -0400

    it's massive

--- script/canvas.js
+++ script/canvas.js
@@ -1,115 +1,107 @@
 // get canvas element
-const canvas = document.getElementById("main-canvas");
+const surface = document.getElementById("surface");
+const overlay = document.getElementById("overlay");
 
 // canvas update on window resize
 function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    surface.width = window.innerWidth;
+    surface.height = window.innerHeight;
+    overlay.width = window.innerWidth;
+    overlay.height = window.innerHeight;
 }
 resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
 
 // drawing context
 /** @type {CanvasRenderingContext2D} */
-const c = canvas.getContext("2d");
+const c = surface.getContext("2d");
 
 // ball object
 function Ball(x, y) {
     this.x = x;
     this.y = y;
-
-    this.r = Math.random()*40 + 5;
-    this.dx = Math.random()*6 - 3;
-    this.dy = Math.random()*6 - 3;
-    this.q = 10;
+    
+    this.r = Math.random() + 0.1;
+    this.q = Math.random() * 4 + 1;
+    
+    this.dx = 0;
+    this.dy = 0;
     
     this.red = Math.floor(Math.random()*256);
     this.green = Math.floor(Math.random()*256);
     this.blue = Math.floor(Math.random()*256);
-
-    this.draw = function() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.r, 0, Math.PI*2);
-        c.fillStyle = "rgba("+this.red+","+this.green+","+this.blue+",0.4)";
-        c.fill();
-    };
-
-    this.update = function() {
-        this.x += this.dx;
-        this.y += this.dy;
-        this.dx += Math.random()*4 - 2;
-        this.dy += Math.random()*4 - 2;
-        if (this.x + this.r > canvas.width) {
-            this.x = canvas.width - this.r;
-            this.dx = -this.dx/this.q;
-        }
-        else if (this.x - this.r < 0) {
-            this.x = this.r;
-            this.dx = -this.dx/this.q;
-        }
-        if (this.y + this.r > canvas.height) {
-            this.y = canvas.height - this.r;
-            this.dy = -this.dy/this.q;
-        }
-        else if (this.y - this.r < 0) {
-            this.y = this.r;
-            this.dy = -this.dy/this.q;
-        }
-    };
 }
 
+Ball.prototype.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.r, 0, Math.PI*2);
+    c.fillStyle = "rgb("+this.red+","+this.green+","+this.blue+")";
+    c.fill();
+};
+
+Ball.prototype.update = function() {
+    this.x += this.dx;
+    this.y += this.dy;
+    this.dx += (Math.random() - 0.5)/10;
+    this.dy += (Math.random() - 0.5)/10;
+    if (this.x + this.r > surface.width) {
+        this.x = surface.width - this.r;
+        this.dx = -this.dx/this.q;
+    }
+    else if (this.x - this.r < 0) {
+        this.x = this.r;
+        this.dx = -this.dx/this.q;
+    }
+    if (this.y + this.r > surface.height) {
+        this.y = surface.height - this.r;
+        this.dy = -this.dy/this.q;
+    }
+    else if (this.y - this.r < 0) {
+        this.y = this.r;
+        this.dy = -this.dy/this.q;
+    }
+};
+
 // track mouse location
-let mouseX = 0;
-let mouseY = 0;
-canvas.addEventListener("mousemove", function(e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+let cursorX = 0;
+let cursorY = 0;
+surface.addEventListener("mousemove", function(e) {
+    cursorX = e.clientX;
+    cursorY = e.clientY;
 });
 
 // ball spawner
 let balls = [];
 function spawnBall() {
-    if (balls.length > 100) {
+    if (balls.length > 750) {
         balls.shift();
     }
-    balls.push(new Ball(mouseX, mouseY));
+    balls.push(new Ball(cursorX, cursorY));
 }
 
 // user interaction
-let id = null;
-canvas.addEventListener("mousedown", function() {
+let id;
+surface.addEventListener("click", spawnBall);
+surface.addEventListener("mousedown", function() {
     id = setInterval(spawnBall, 10);
 });
-canvas.addEventListener("mouseup", function() {
+surface.addEventListener("mouseup", function() {
     clearInterval(id);
 });
-canvas.addEventListener("mouseleave", function() {
+surface.addEventListener("mouseleave", function() {
     clearInterval(id);
 });
-canvas.addEventListener("click", spawnBall);
 
 // animator
 function animate() {
-    requestAnimationFrame(animate);
-    let t = Date.now() % 20000;
-    if (t < 10000) {
-        if (t < 2000) {
-            c.fillStyle = "rgba(0, 0, 0,"+(t/2000)+")";
-            c.fillRect(0, 0, canvas.width, canvas.height);
-        }
-        else {
-            c.clearRect(0, 0, canvas.width, canvas.height);
-        }
-    }
-    else if (t < 12000) {
-        c.fillStyle = "rgba(0, 0, 0,"+(1-(t-10000)/2000)+")";
-        c.fillRect(0, 0, canvas.width, canvas.height);
-    }
+    c.fillStyle = "rgba(0, 0, 0, 0.1)";
+    c.fillRect(0, 0, surface.width, surface.height);
+
     balls.forEach(b => {
         b.draw();
         b.update();
     });
-    c.fillStyle = "rgba(0, 0, 0, 0.4)";
-    c.fillRect(14, 14, 300, 100);
+
+    requestAnimationFrame(animate);
 }
 animate();
